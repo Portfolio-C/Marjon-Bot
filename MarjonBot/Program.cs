bc28584839211d0c99345b68271b1cd456722589
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-04-09 23:08:18 +0500

    quick start

--- MarjonBot/Program.cs
+++ MarjonBot/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient("7504461015:AAHk4U930xCdvDEdIMTLE6XD_4R4kuqqU3I", cancellationToken: cts.Token);
+var me = await bot.GetMe();
+bot.OnMessage += OnMessage;
+
+Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
+Console.ReadLine();
+cts.Cancel();
+
+async Task OnMessage(Message msg, UpdateType type)
+{
+    if (msg.Text is null) return;
+    Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
+
+    await bot.SendMessage(msg.Chat, $"{msg.From} said: {msg.Text}");
+}
